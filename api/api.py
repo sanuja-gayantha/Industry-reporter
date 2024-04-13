a580e9a2c55ac41733580bbe92b08155d64f2714
sanuja gayantha <rpsgayantha@gmail.com>
2024-04-13 19:04:10 +0530

    commit

--- api/api.py
+++ api/api.py
@@ -2,7 +2,15 @@
 
 
 
+class Api():
+    def __init__(self):
+        self.pdf_data_list_path = os.path.join(os.getcwd(), 'pdfDataList.json')
 
+    
+    def read_json_file(self, path):
+        with open(path) as json_file:
+            result = json.load(json_file)
+            return result
 
 
 
