3073d6f0c7b63dcb3a6f31ab89f1c217ee952d81
sanuja gayantha <rpsgayantha@gmail.com>
2024-04-14 09:53:09 +0530

    commit

--- api/api.py
+++ api/api.py
@@ -9,62 +9,61 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-from .constants import SPREADSHEET_ID
+from .constants import SCOPES, SPREADSHEET_ID
 
 
-# class Api():
-#     def __init__(self):
-#         self.pdf_data_list_path = os.path.join(os.getcwd(), 'pdfDataList.json')
+class Api():
+    def __init__(self):
+        self.credentials = None
+        self.SCOPES = [SCOPES]
+        self.authentication()
+
+
+    def authentication(self):
+        if os.path.exists("./api/token.json"):
+            self.credentials = Credentials.from_authorized_user_file("./api/token.json", self.SCOPES)
+
+        if not self.credentials or not self.credentials.valid:
+            if self.credentials and self.credentials.expired and self.credentials.refresh_token:
+                self.credentials.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "./api/credentials.json", self.SCOPES
+                )
+                self.credentials = flow.run_local_server(port=0)
+                # Save the credentials for the next run
+                with open("./api/token.json", "w") as token:
+                    token.write(self.credentials.to_json())
+
+
+    def api_read_spreadsheet(self):
+        try:
+            service = build("sheets", "v4", credentials=self.credentials)
+            sheet = service.spreadsheets()
+
+            result = (
+                sheet.values()
+                .get(spreadsheetId=SPREADSHEET_ID, range="Details!A2:E")
+                .execute())
+            
+            values = result.get("values", [])
+            temp_pdf_links=[]
+            for row in values:
+                temp_pdf_links.append(row[3])
+            return temp_pdf_links
+  
+        except HttpError as err:
+            print(err)
     
-#     def read_json_file(self, path):
-#         with open(path) as json_file:
-#             result = json.load(json_file)
-#             return result
 
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-
-def api_main():
-    # pdf_data_list_path = os.path.join(os.getcwd(), 'pdfDataList.json')
-
-    # ins = Api()
-    # print(ins.read_json_file(pdf_data_list_path))
-
-    credentials = None
-    if os.path.exists("./api/token.json"):
-        credentials = Credentials.from_authorized_user_file("./api/token.json", SCOPES)
-
-    if not credentials or not credentials.valid:
-        if credentials and credentials.expired and credentials.refresh_token:
-            credentials.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "./api/credentials.json", SCOPES
-            )
-            credentials = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open("./api/token.json", "w") as token:
-                token.write(credentials.to_json())
+    def api_append_spreadsheet(self):
 
 
-    try:
-        service = build("sheets", "v4", credentials=credentials)
-        sheet = service.spreadsheets()
-
-        result = (
-            sheet.values()
-            .get(spreadsheetId=SPREADSHEET_ID, range="Details!A2:E")
-            .execute())
-        
-        values = result.get("values", [])
-
-        for row in values:
-            print(row)
-        # Print columns A and E, which correspond to indices 0 and 4.
-        # print(f"{row[0]}, {row[4]}")
 
 
-    except HttpError as err:
-        print(err)
+
+
+
 
 
 
--- api/constants.py
+++ api/constants.py
@@ -1,5 +1,6 @@
 # Developed by @sanuja : https://github.com/sanuja-gayantha
 
+SCOPES = "https://www.googleapis.com/auth/spreadsheets"
 SPREADSHEET_ID = "16t6bSxGB0wUz7w18cZKrJ6nM9EyyvsuOMDQXr_guFGE"
 
 
